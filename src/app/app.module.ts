a161dc9b0dbf8d53acac7adfb213856c8f8f82ff
nompilo <nonnyenhle@gmail.com>
2018-08-20 13:20:28 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { InfoPage } from './../pages/info/info';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -6,25 +8,30 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { CourseServiceProvider } from '../providers/course-service/course-service';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    InfoPage,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    InfoPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CourseServiceProvider,
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
+import { InfoPage } from './../info/info';
+import { CourseServiceProvider } from './../../providers/course-service/course-service';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+ selector: 'page-home',
+ templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+courses:any
+arrCourses:any
+ constructor(public navCtrl: NavController,private courseProvider:CourseServiceProvider) {
+courseProvider.getCourses().subscribe(results => {
+ this.courses= results;
+ this.arrCourses=this.courses.courses
+ console.log("response",results)
+});
+}
+next(course:any){
+  this.navCtrl.push(InfoPage,{value:course});
+}
 }
